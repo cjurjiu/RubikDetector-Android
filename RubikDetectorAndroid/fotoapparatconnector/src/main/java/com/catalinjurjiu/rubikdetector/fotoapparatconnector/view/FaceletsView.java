2a824a84d9329c6ff512b274d3ad8cb19af3ea6d
cjurjiu <cjurjiu@users.noreply.github.com>
2017-09-30 09:03:55 +0300

    Added attributes which allow drawing to be customized through XML, for the FotoApparat RubikDetectorRestulView

--- RubikDetectorAndroid/fotoapparatconnector/src/main/java/com/catalinjurjiu/rubikdetector/fotoapparatconnector/view/FaceletsView.java
+++ RubikDetectorAndroid/fotoapparatconnector/src/main/java/com/catalinjurjiu/rubikdetector/fotoapparatconnector/view/FaceletsView.java
@@ -4,16 +4,18 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.catalinjurjiu.rubikdetector.RubikDetectorUtils;
+import com.catalinjurjiu.rubikdetector.config.DrawConfig;
 import com.catalinjurjiu.rubikdetector.fotoapparatconnector.data.RubikResultWrapper;
+import com.catalinjurjiu.rubikdetector.model.Point2d;
 import com.catalinjurjiu.rubikdetector.model.RubikFacelet;
-
-import static com.catalinjurjiu.rubikdetector.RubikDetectorUtils.getAndroidColor;
 
 /**
  * Created by catalin on 01.09.2017.
@@ -21,35 +23,50 @@ import static com.catalinjurjiu.rubikdetector.RubikDetectorUtils.getAndroidColor
 class FaceletsView extends View {
     private RubikResultWrapper detectionResult;
     private Paint paint;
+    private DrawConfig drawConfig;
     private int resultFrameWidth;
     private int resultFrameHeight;
     private int resultFrameSmallSide;
+    private Path path;
 
-    public FaceletsView(Context context) {
+    public FaceletsView(Context context, DrawConfig drawConfig) {
         super(context);
-        init();
+        init(drawConfig);
     }
 
     public FaceletsView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(DrawConfig.Default());
     }
 
     public FaceletsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(DrawConfig.Default());
     }
 
-    private void init() {
+    private void init(DrawConfig drawConfig) {
+        this.drawConfig = drawConfig;
+        if (drawConfig.getDrawMode() == DrawConfig.DrawMode.DRAW_RECTANGLES) {
+            path = new Path();
+        }
         paint = new Paint();
         paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(10);
+        paint.setStyle(drawConfig.isFillShape() ? Paint.Style.FILL_AND_STROKE : Paint.Style.STROKE);
+        paint.setStrokeWidth(drawConfig.getStrokeWidth());
         paint.setColor(Color.DKGRAY);
     }
 
     public void displayFacelets(@NonNull RubikResultWrapper rubikResult) {
-        ensureMainThread();
+
+        if (Looper.getMainLooper() != Looper.myLooper()) {
+            throw new IllegalThreadStateException("This method must be called from the main thread");
+        }
+
+        if (drawConfig.getDrawMode() == DrawConfig.DrawMode.DO_NOT_DRAW) {
+            //do nothing if the user doesn't want to draw the result
+            return;
+        }
+
         this.detectionResult = rubikResult;
         invalidate();
     }
@@ -58,38 +75,75 @@ class FaceletsView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (detectionResult != null && detectionResult.getDetectedFacelets() != null) {
-
-            if (resultFrameWidth != detectionResult.getFrameWidth() || resultFrameHeight != detectionResult.getFrameHeight()) {
-                resultFrameWidth = detectionResult.getFrameWidth();
-                resultFrameHeight = detectionResult.getFrameHeight();
-                resultFrameSmallSide = Math.min(resultFrameWidth, resultFrameHeight);
-            }
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    RubikFacelet facelet = detectionResult.getDetectedFacelets()[i][j];
-                    paint.setColor(getAndroidColor(facelet));
-
-                    float xRatio = facelet.center.x / resultFrameWidth;
-                    float yRatio = facelet.center.y / resultFrameHeight;
-                    float radiusRatio = Math.min(facelet.width, facelet.height) / resultFrameSmallSide;
-
-                    canvas.drawCircle(
-                            xRatio * getWidth(),
-                            yRatio * getHeight(),
-                            (radiusRatio * Math.min(getHeight(), getWidth())) / 2f,
-                            paint
-                    );
-                }
-            }
-
+        if (detectionResult == null || detectionResult.getDetectedFacelets() == null || drawConfig.getDrawMode() == DrawConfig.DrawMode.DO_NOT_DRAW) {
+            return;
+        }
+        if (drawConfig.getDrawMode() == DrawConfig.DrawMode.DRAW_RECTANGLES) {
+            drawFaceletsAsRectangles(canvas);
+        } else {
+            drawFaceletsAsCircles(canvas);
         }
     }
 
-    private void ensureMainThread() {
-        if (Looper.getMainLooper() != Looper.myLooper()) {
-            throw new IllegalThreadStateException("This method must be called from the main thread");
+    private void drawFaceletsAsCircles(Canvas canvas) {
+        if (resultFrameWidth != detectionResult.getFrameWidth() || resultFrameHeight != detectionResult.getFrameHeight()) {
+            resultFrameWidth = detectionResult.getFrameWidth();
+            resultFrameHeight = detectionResult.getFrameHeight();
+            resultFrameSmallSide = Math.min(resultFrameWidth, resultFrameHeight);
         }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                RubikFacelet facelet = detectionResult.getDetectedFacelets()[i][j];
+                paint.setColor(RubikDetectorUtils.getAndroidColor(facelet));
+                float radiusRatio = Math.min(facelet.width, facelet.height) / resultFrameSmallSide;
+
+                canvas.drawCircle(
+                        getScaledXValue(facelet.center.x),
+                        getScaledYValue(facelet.center.y),
+                        (radiusRatio * Math.min(getHeight(), getWidth())) / 2f,
+                        paint);
+            }
+        }
+    }
+
+    private void drawFaceletsAsRectangles(Canvas canvas) {
+        if (resultFrameWidth != detectionResult.getFrameWidth() || resultFrameHeight != detectionResult.getFrameHeight()) {
+            resultFrameWidth = detectionResult.getFrameWidth();
+            resultFrameHeight = detectionResult.getFrameHeight();
+            resultFrameSmallSide = Math.min(resultFrameWidth, resultFrameHeight);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                RubikFacelet facelet = detectionResult.getDetectedFacelets()[i][j];
+                Point2d[] points = detectionResult.getDetectedFacelets()[i][j].getPoints();
+
+                paint.setColor(RubikDetectorUtils.getAndroidColor(facelet));
+                path.reset();
+
+                path.moveTo(getScaledXValue(points[0].x), getScaledYValue(points[0].y));
+                path.lineTo(getScaledXValue(points[1].x), getScaledYValue(points[1].y));
+
+                path.moveTo(getScaledXValue(points[1].x), getScaledYValue(points[1].y));
+                path.lineTo(getScaledXValue(points[2].x), getScaledYValue(points[2].y));
+
+                path.moveTo(getScaledXValue(points[2].x), getScaledYValue(points[2].y));
+                path.lineTo(getScaledXValue(points[3].x), getScaledYValue(points[3].y));
+
+                path.moveTo(getScaledXValue(points[3].x), getScaledYValue(points[3].y));
+                path.lineTo(getScaledXValue(points[0].x), getScaledYValue(points[0].y));
+
+                canvas.drawPath(path, paint);
+            }
+        }
+    }
+
+    private float getScaledXValue(float xValue) {
+        return xValue / resultFrameWidth * getWidth();
+    }
+
+    private float getScaledYValue(float yValue) {
+        return yValue / resultFrameHeight * getHeight();
     }
 }
--- RubikDetectorAndroid/fotoapparatconnector/src/main/java/com/catalinjurjiu/rubikdetector/fotoapparatconnector/view/RubikDetectorResultView.java
+++ RubikDetectorAndroid/fotoapparatconnector/src/main/java/com/catalinjurjiu/rubikdetector/fotoapparatconnector/view/RubikDetectorResultView.java
@@ -1,7 +1,9 @@
 package com.catalinjurjiu.rubikdetector.fotoapparatconnector.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.AttrRes;
+import android.support.annotation.Dimension;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -10,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import com.catalinjurjiu.rubikdetector.config.DrawConfig;
+import com.catalinjurjiu.rubikdetector.fotoapparatconnector.R;
 import com.catalinjurjiu.rubikdetector.fotoapparatconnector.RubikDetectorFrameProcessor;
 import com.catalinjurjiu.rubikdetector.fotoapparatconnector.data.RubikResultWrapper;
 
@@ -18,40 +22,78 @@ import io.fotoapparat.view.CameraView;
 /**
  * Created by catalin on 29.08.2017.
  */
-
 public class RubikDetectorResultView extends FrameLayout implements RubikDetectorFrameProcessor.OnRubikCubeFaceletsFoundListener {
 
+    public static final String FILLED_CIRCLES = "filledCircles";
+    public static final String CIRCLES = "circles";
+    public static final String RECTANGLES = "rectangles";
+    public static final String DO_NOT_DRAW = "doNotDraw";
+    @Dimension
+    private static final int STROKE_NOT_SET = -2;
     private CameraView cameraView;
 
     private FaceletsView faceletsView;
 
     public RubikDetectorResultView(@NonNull Context context) {
         super(context);
-        initView(context);
+        initView(context, null);
     }
 
     public RubikDetectorResultView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initView(context);
+        initView(context, attrs);
     }
 
     public RubikDetectorResultView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initView(context);
+        initView(context, attrs);
     }
 
     public CameraView getCameraView() {
         return cameraView;
     }
 
-    private void initView(Context context) {
+    private void initView(@NonNull Context context, @Nullable AttributeSet attrs) {
         this.cameraView = new CameraView(context);
         cameraView.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         addView(cameraView);
 
-        this.faceletsView = new FaceletsView(context);
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.RubikDetectorResultView);
+        DrawConfig drawConfig = createDrawConfig(attributes);
+        attributes.recycle();
+
+        this.faceletsView = new FaceletsView(context, drawConfig);
         faceletsView.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         addView(faceletsView);
+    }
+
+    @NonNull
+    private DrawConfig createDrawConfig(TypedArray attributes) {
+        String faceletsDrawMode = attributes.getString(R.styleable.RubikDetectorResultView_faceletsDrawMode);
+        int strokeWidthInPixels = attributes.getDimensionPixelSize(R.styleable.RubikDetectorResultView_strokeWidth, STROKE_NOT_SET);
+
+        DrawConfig drawConfig;
+        if (faceletsDrawMode == null) {
+            drawConfig = DrawConfig.Default();
+        } else {
+            switch (faceletsDrawMode) {
+                case FILLED_CIRCLES:
+                    drawConfig = DrawConfig.FilledCircles();
+                    break;
+                case CIRCLES:
+                    drawConfig = DrawConfig.Circles(strokeWidthInPixels);
+                    break;
+                case RECTANGLES:
+                    drawConfig = DrawConfig.Rectangles(strokeWidthInPixels);
+                    break;
+                case DO_NOT_DRAW:
+                    drawConfig = DrawConfig.DoNotDraw();
+                    break;
+                default:
+                    drawConfig = DrawConfig.Default();
+            }
+        }
+        return drawConfig;
     }
 
     @Override
@@ -61,7 +103,6 @@ public class RubikDetectorResultView extends FrameLayout implements RubikDetecto
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-
         final View textureView = findTextureView(cameraView);
 
         cameraView.layout(left, top, right, bottom);
